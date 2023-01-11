119223793277d185870b923ac956364f8985c66c
Thosam1 <thosamtenzin2002@gmail.com>
2023-01-11 20:28:14 +0100

    save and unsave post return data

--- src/service/post.service.ts
+++ src/service/post.service.ts
@@ -1,5 +1,6 @@
 import { Ref } from "@typegoose/typegoose";
 import PostModel, { Post } from "../model/post.model";
+import { User } from "../model/user.model";
 
 export async function createPost(input: {
   user: string;
@@ -21,6 +22,10 @@ export async function findPostById(id: string | Ref<Post>) {
 export async function deletePostById(id: string) {
     return PostModel.deleteOne({ _id: id });
 }
+
+// export async function omitAndAppendUserInfo(post: Post) {
+//   return PostModel.deleteOne({ _id: id });
+// }
 
 // this one we create a route with a handler
 // export async function addPostToUserSavedPosts(postID: string, userID: string) {
